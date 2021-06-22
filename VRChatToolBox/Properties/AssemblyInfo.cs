89f12cc42ac5855c0b08b422f25637cd57fd980b
taroufujimoto <taroufujimoto075@gmail.com>
2021-06-22 21:37:34 +0900

    著作権表記を変更

--- VRChatToolBox/Properties/AssemblyInfo.cs
+++ VRChatToolBox/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Macrohard")]
 [assembly: AssemblyProduct("VRChatToolBox")]
-[assembly: AssemblyCopyright("MInaduki Sekina Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2021 Minaduki Sekina")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
